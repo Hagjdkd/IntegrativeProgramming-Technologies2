e28134216d419992ac7d18f3de6efd15c7319f8e
Hagjdkd <villarinoemman030@gmail.com>
2026-03-22 16:06:18 +0800

    Activity2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,24 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  name = 'Emman Villarino';
-  age = 20;
-  section = 'BSIT-3A';
+ username = 'admin';
+ password = 'admin123';
+
+ login() {
+   // Implement login logic here
+   if (this.username === 'admin' && this.password === 'admin123') {
+     alert('Login successful!');
+   } else {
+     alert('Invalid username or password.');
+   }
+ }
 }
