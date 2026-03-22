83d5462f78096a834fd44591e1ebbf14b7eacaee
Hagjdkd <villarinoemman030@gmail.com>
2026-03-22 15:42:08 +0800

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Activity-1');
+  name = 'Emman Villarino';
+  age = 20;
+  section = 'BSIT-3A';
 }
