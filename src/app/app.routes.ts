10a91c5cff051df4a89535b5a8eacb6fa5a37d27
Hagjdkd <villarinoemman030@gmail.com>
2026-04-13 18:30:10 +0800

    Added folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login'; 
+import { Portfolio } from './portfolio/portfolio';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'dashboard', component: Dashboard },
-  // Change '/dashboard' to '/login' below:
+  {path: 'portfolio', component: Portfolio},
   { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
